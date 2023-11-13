e19b911cff12749b381bdc2d004b1e8b65ea47df
samweb515 <samuel.webster515@gmail.com>
2023-11-13 16:46:34 -0500

    aze done

--- Assets/CellCluster.cs
+++ Assets/CellCluster.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CellCluster : MonoBehaviour
 {
+	public int x;
+	public int z;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Maze.cs
+++ Assets/Maze.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -12,9 +13,11 @@ public class Maze : MonoBehaviour {
 
 	public void Generate () {
 		float neighbortriname;
+		float wallname;
 		int neighbortri;
-		sizeX = 4;
-		sizeZ = 4;
+		float triname;
+		sizeX = 16;
+		sizeZ = 16;
 		cellclusters = new CellCluster[sizeX, sizeZ];
 		for (int x = 0; x < sizeX; x++) {
 			for (int z = 0; z < sizeZ; z++) {
@@ -25,10 +28,7 @@ public class Maze : MonoBehaviour {
 			for (int z = 0; z < sizeZ; z++) {
 				for (int tri = 0; tri < 4; tri++) {
 					// find target cell
-					float triname = Mathf.Pow(2, tri);
-					Debug.Log(cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>()); // original triangle
-					
-            		Debug.Log(System.Convert.ToString(tri, toBase: 2)); //original triangle in binary
+					triname = Mathf.Pow(2, tri);
             		neighbortri = tri ^ 2;
 					neighbortriname = Mathf.Pow(2, neighbortri);
             		cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors.Add(cellclusters[x, z].transform.Find(""+neighbortriname).GetComponent<TriangleCellFloor>()); // prints out value with xy flag flipped
@@ -40,48 +40,54 @@ public class Maze : MonoBehaviour {
 						if ((tri & (1U << 1)) != 0) // check xz flag
             			{
 							if ((tri & (1U << 0)) == 0) {
-								cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors.Add(cellclusters[x, z + 1].transform.Find(""+neighbortriname).GetComponent<TriangleCellFloor>());
-							}
-							else {
-								cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors.Add(cellclusters[x, z - 1].transform.Find(""+neighbortriname).GetComponent<TriangleCellFloor>());
-							}
-            			}
-            			else // triangle is on x axis
-            			{
-                			if ((tri & (1U << 0)) == 0) {
 								cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors.Add(cellclusters[x + 1, z].transform.Find(""+neighbortriname).GetComponent<TriangleCellFloor>());
 							}
 							else {
 								cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors.Add(cellclusters[x - 1, z].transform.Find(""+neighbortriname).GetComponent<TriangleCellFloor>());
 							}
             			}
+            			else // triangle is on x axis
+            			{
+                			if ((tri & (1U << 0)) == 0) {
+								cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors.Add(cellclusters[x, z + 1].transform.Find(""+neighbortriname).GetComponent<TriangleCellFloor>());
+							}
+							else {
+								cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors.Add(cellclusters[x, z - 1].transform.Find(""+neighbortriname).GetComponent<TriangleCellFloor>());
+							}
+            			}
 					} catch (Exception) {
-						Debug.Log("edge cell");
+
 					}
+					// WIP
+        			List<TriangleCellFloor> temp = new List<TriangleCellFloor>();
+        			int j = cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors.Count;
+        			for (int i = 0; i < j; i++)
+        			{
+            			temp.Add(cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors[UnityEngine.Random.Range(0, cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors.Count)]);
+            			cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors.Remove(temp.Last());
+        			}
+        			cellclusters[x, z].transform.Find(""+triname).GetComponent<TriangleCellFloor>().neighbors = temp;
 				}
 			}
 		}
 		Stack<TriangleCellFloor> work = new Stack<TriangleCellFloor> ();
 		List<TriangleCellFloor> visited = new List<TriangleCellFloor> ();
 
-		TriangleCellFloor start = cellclusters[0, 0].transform.Find("1").GetComponent<TriangleCellFloor>();
-		TriangleCellFloor end = cellclusters[1, 1].transform.Find("1").GetComponent<TriangleCellFloor>();
+		TriangleCellFloor start = cellclusters[0, 0].transform.Find("2").GetComponent<TriangleCellFloor>();
+		//TriangleCellFloor end = cellclusters[1, 1].transform.Find("2").GetComponent<TriangleCellFloor>();
 
 		work.Push (start);
 		Debug.Log(work);
 		visited.Add (start);
-		start.history = new List<TriangleCellFloor> ();
 
 		while(work.Count > 0){
 
 			TriangleCellFloor current = work.Pop ();
-			if (current == end) {
-				List<TriangleCellFloor> result = current.history;
-				result.Add (current);
-				Debug.Log(result);
-				break;
+			if (false) { //(current == end) {
+				//List<TriangleCellFloor> result = current.history;
+				//result.Add (current);
+				//Debug.Log(result);
 			} else {
-			
 				for(int i = 0; i < current.neighbors.Count; i++){
 
 					TriangleCellFloor currentChild = current.neighbors [i];
@@ -89,10 +95,39 @@ public class Maze : MonoBehaviour {
 
 						work.Push (currentChild);
 						visited.Add (currentChild);
-						currentChild.history = new List<TriangleCellFloor> (current.history);
+						currentChild.history.AddRange (current.history);
 						currentChild.history.Add (current);
 					}
 				}	
+			}
+		}
+		for (int x = 0; x < sizeX; x++)
+		{
+			for (int z = 0; z < sizeZ; z++)
+			{
+				for (int tri = 0; tri < 4; tri++)
+				{
+					triname = Mathf.Pow(2, tri);
+					TriangleCellFloor neighbor1 = cellclusters[x, z].transform.Find("" + triname).GetComponent<TriangleCellFloor>();
+					if (neighbor1 != start) {
+						TriangleCellFloor neighbor2 = cellclusters[x, z].transform.Find("" + triname).GetComponent<TriangleCellFloor>().history.Last();
+						wallname = float.Parse(neighbor1.name) + float.Parse(neighbor2.name);
+						if (neighbor2.transform.parent.GetComponent<CellCluster>().x == x && neighbor2.transform.parent.GetComponent<CellCluster>().z == z) {
+							//Debug.Log("Destroying"+ " at " + neighbor2.transform.parent.GetComponent<CellCluster>().x + ", " + neighbor2.transform.parent.GetComponent<CellCluster>().z);
+							Destroy(cellclusters[x, z].transform.Find("" + wallname).gameObject);
+						} else if (Mathf.Log(float.Parse(neighbor1.name), 2) % 2 != 0) {
+							Debug.Log("n1: " + cellclusters[x, z].transform.Find("" + triname).GetComponent<TriangleCellFloor>().name + " at " + x + ", " + z);
+							Debug.Log("n2: " + cellclusters[x, z].transform.Find("" + triname).GetComponent<TriangleCellFloor>().history.Last().name + " at " + neighbor2.transform.parent.GetComponent<CellCluster>().x + ", " + neighbor2.transform.parent.GetComponent<CellCluster>().z);
+							Debug.Log("wallname: " + wallname);
+							Destroy(cellclusters[x, z].transform.Find("" + wallname).gameObject);
+						} else {
+							Debug.Log("n1: " + cellclusters[x, z].transform.Find("" + triname).GetComponent<TriangleCellFloor>().name);
+							Debug.Log("n2: " + cellclusters[x, z].transform.Find("" + triname).GetComponent<TriangleCellFloor>().history.Last().name);
+							Debug.Log("wallname: " + wallname);
+							Destroy(cellclusters[neighbor2.transform.parent.GetComponent<CellCluster>().x, neighbor2.transform.parent.GetComponent<CellCluster>().z].transform.Find("" + wallname).gameObject);
+						} //greater one holds walls
+					}
+				}
 			}
 		}
 	}
@@ -104,6 +139,8 @@ public class Maze : MonoBehaviour {
 	private void CreateCell (int x, int z) {
 		CellCluster newCell = Instantiate(cellPrefab) as CellCluster;
 		
+		newCell.x = x;
+		newCell.z = z;
 		cellclusters[x, z] = newCell;
 		newCell.name = "Maze Cell " + x + ", " + z;
 		newCell.transform.localPosition = new Vector3(x - sizeX * 0.5f + 0.5f, 0f, z - sizeZ * 0.5f + 0.5f);
